681576cfafdf030bb2e9112c5be13a55a3048db9
udani95 <39646429+udani95@users.noreply.github.com>
2020-05-10 00:06:56 +0530

    small string exercise + type() + str() functions
    Do all of this in a .py file in Pycharm
    
    Create a variable and assign it the string "Just do it!"
    
    Access the "!" from the variable by index and print() it
    
    Print the slice "do" from the variable
    
    Get and print the slice "it!" from the variable
    
    Print the slice "Just" from the variable
    
    Get the string slice "do it!" from the variable and concatenate it with the string "Don't ".  Print the resulting string.

---
+++ String exercises and type and str function.py
@@ -0,0 +1,34 @@
+
+
+var1 = "Just do it!"
+print(var1[10]) #print !
+print(var1[5:7]) #print do
+print(var1[8:]) #print it!
+print(var1[:4]) #print Just
+print("Don't " + var1[5:]) #print Don't do it
+
+#how to get the type of a variable
+
+print("\n\n Printing all variable's types ")
+var11 = True
+var2 = 2
+var3 = 2.5
+var4 = "String"
+
+print(type(var11))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+
+#how to convert a variable into a string type
+
+print("\n\n converting all variables into str type ")
+var11 = str(True)
+var2 = str(2)
+var3 = str(2.5)
+var4 = str("String")
+
+print(type(var11))# all variables are convert in to string type
+print(type(var2))
+print(type(var3))
+print(type(var4))
