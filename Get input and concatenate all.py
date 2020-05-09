0cb86502014d940fa98f49c8203f870342e03d7a
Udani Kaushalya <39646429+udani95@users.noreply.github.com>
2020-05-10 01:17:38 +0530

    Get a input from the user and concatenate all the answers into a one sentence and print it
    In a .py file, create a program and use input() three times to get answers to the following questions from a user.  Store each of the answers in a variable.
    
    What is your name?
    
    What is your quest?
    
    What is your favorite color?
    
    Then, concatenate everything into a string within a print() statement with the form "So your name is [name], your quest is [quest], and your favorite color is [color]."

---
+++ Get input and concatenate all.py
@@ -0,0 +1,6 @@
+
+var1= input("What is your name?")
+var2= input("What is your quest?")
+var3= input("What is your favorite color?")
+
+print("So your name is " +  var1 + ", your quest is " +var2 +", and your favorite color is "+var3)
