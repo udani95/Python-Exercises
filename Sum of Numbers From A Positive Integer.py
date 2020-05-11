c0b7bf043faf9384969d0d87f705238621ca30a8
Udani Kaushalya <39646429+udani95@users.noreply.github.com>
2020-05-11 22:24:01 +0530

    Create Sum of Numbers From A Positive Integer.py
    Programming Challenge: Sum of Numbers From A Positive Integer
    Write a program which gets a positive integer from a user using input() and assigns it to a variable.
    
    The program should then use a while loop to get the sum of all of the integers from the integer that was entered by the user down to 1.  For example, if the integer entered by the user was 6, the while loop would find the sum of 6, 5, 4, 3, 2, and 1, which is 21.
    
    At the bottom of your program create two print statements.  One will display the user entered integer and the other will display the sum found by the while loop.

---
+++ Sum of Numbers From A Positive Integer.py
@@ -0,0 +1,8 @@
+var1=int(input("Enter a number===="))
+sum =0
+print("Entered Integer=" + str(var1))
+while var1 >= 1:
+    sum= sum+var1
+    var1 -= 1
+
+print("\n\nSummation of the integer in a the while loop=" + str(sum))
