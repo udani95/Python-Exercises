5233ce28d406b08a9057480aab5b9e571b92b14e
Udani Kaushalya <39646429+udani95@users.noreply.github.com>
2020-05-11 22:03:05 +0530

    Create Roman Numeral Equivalent.py
    For this exercise, start by creating a variable and assigning it a randomly generated integer between and inclusive of both 1 and 10.
    
    Then, using your knowledge of if, elif, and else statements, create a program which prints the roman numeral equivalent of the randomly generated number.
    
    For example, if the randomly generated integer was 9, then the program would say that the roman numeral equivalent of 9 is IX in the output.

---
+++ Roman Numeral Equivalent.py
@@ -0,0 +1,24 @@
+from random import randint
+var1= randint(1,10)
+print("Your random generated number = " + str(var1))
+if var1 == 1:
+    print("The roman numeral equivalent of 1 is I in the output.")
+elif var1 == 2:
+    print("The roman numeral equivalent of 2 is II in the output.")
+elif var1 == 3:
+    print("The roman numeral equivalent of 3 is III in the output.")
+elif var1 == 4:
+    print("The roman numeral equivalent of 4 is IV in the output.")
+elif var1 == 5:
+    print("The roman numeral equivalent of 5 is V in the output.")
+elif var1 == 6:
+    print("The roman numeral equivalent of 6 is VI in the output.")
+elif var1 == 7:
+    print("The roman numeral equivalent of 7 is VII in the output.")
+elif var1 == 8:
+    print("The roman numeral equivalent of 8 is VIII in the output.")
+elif var1 == 9:
+    print("The roman numeral equivalent of 9 is IX in the output.")
+elif var1 == 10:
+    print("The roman numeral equivalent of 10 is X in the output.")
+
